82db5b7f293f4c0d52f18a102ca4ec1496a501a0
Carl-Erwin Griffith <carl.erwin@gmail.com>
2018-02-11 15:54:11 +0100

    cleanup

--- app/eedit/test-tools/gen_lines.rs
+++ app/eedit/test-tools/gen_lines.rs
@@ -17,11 +17,11 @@ fn main()
   gen_lines(start_num, stop_num, width_num);
 }
 
-fn gen_lines(start: u64, stop: u64, linewidth: u64) -> ()
+fn gen_lines(start: u64, stop: u64, linewidth: u64)
 {
   let string = gen_line(linewidth);
 
-  for x in (start..start+stop+1) {
+  for x in start..start+stop+1 {
     print!("{:012} {}", x, string);
   }
 }
@@ -33,7 +33,7 @@ fn gen_line(linewidth : u64) -> String
   let table = [ '0','1','2','3','4','5','6','7','8','9',
                 'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' ];
 
-  for x  in (0..linewidth) {
+  for x  in 0..linewidth {
       string.push(table[x as usize % table.len() ]);
   }
   string.push('\n');
